f1a023be8df485dd79e8d5208c6ac6fb5f7bf25d
antobyte <78034476+antobyte@users.noreply.github.com>
2025-04-17 22:06:53 +0200

    bang

--- src/telegram.rs
+++ src/telegram.rs
@@ -1,8 +1,12 @@
 // src/telegram.rs
 
-use teloxide::prelude::*;
-use teloxide::dptree;
-use teloxide::utils::command::BotCommands;
+use std::sync::Arc;
+use teloxide::{
+    prelude::*,
+    dptree,
+    types::CallbackQuery,
+    utils::command::BotCommands,
+};
 use crate::notifier::{self, Command};
 use crate::exchange::Exchange;
 
@@ -10,22 +14,56 @@ pub async fn run<E>(bot: Bot, exchange: E)
 where
     E: Exchange + Clone + Send + Sync + 'static,
 {
-    // only handle text messages that parse as our Command
-    let handler = Update::filter_message()
+    // Оборачиваем exchange в Arc, чтобы его можно было клонировать в замыканиях
+    let exchange = Arc::new(exchange);
+
+    // 1) Обработка текстовых команд /help, /status, /hedge, /unhedge
+    let commands_branch = Update::filter_message()
         .filter_command::<Command>()
-        .endpoint(
+        .endpoint({
+            let exchange = exchange.clone();
             move |bot: Bot, msg: Message, cmd: Command| {
-                let ex = exchange.clone();
+                let exchange = exchange.clone();
                 async move {
-                    if let Err(err) = notifier::handler(bot, msg, cmd, ex).await {
-                        tracing::error!("handler error: {:?}", err);
+                    if let Err(err) = notifier::handle_command(
+                        bot.clone(),
+                        msg.clone(),
+                        cmd,
+                        (*exchange).clone(),
+                    )
+                    .await
+                    {
+                        tracing::error!("command handler error: {:?}", err);
                     }
                     respond(())
                 }
-            },
-        );
+            }
+        });
 
-    Dispatcher::builder(bot, dptree::entry().branch(handler))
+    // 2) Обработка inline‑callback кнопок
+    let callback_branch = Update::filter_callback_query()
+        .endpoint({
+            let exchange = exchange.clone();
+            move |bot: Bot, q: CallbackQuery| {
+                let exchange = exchange.clone();
+                async move {
+                    if let Err(err) = notifier::handle_callback(
+                        bot.clone(),
+                        q.clone(),
+                        (*exchange).clone(),
+                    )
+                    .await
+                    {
+                        tracing::error!("callback handler error: {:?}", err);
+                    }
+                    respond(())
+                }
+            }
+        });
+
+    Dispatcher::builder(bot, dptree::entry()
+        .branch(commands_branch)
+        .branch(callback_branch))
         .enable_ctrlc_handler()
         .build()
         .dispatch()
